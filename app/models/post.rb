925f8af585f4954bb65a375e4daf2de7df9df147
EC2 Default User <ec2-user@ip-172-31-45-31.ap-northeast-1.compute.internal>
2025-04-01 08:05:26 +0000

    clean

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,16 +6,5 @@ class Post < ApplicationRecord
   validates :shop_name, presence: true
   validates :shop_address, presence: true
 
-  def self.search_for(content, method)
-    if method == 'perfect'
-      Post.where(title: content)
-    elsif method == 'forward'
-      Post.where('name LIKE ?', content + '%')
-    elsif method == 'backward'
-      Post.where('name LIKE ?', '%' + content)
-    else
-      Post.where('name LIKE ?', '%' + content + '%')
-    end
-  end
 
 end
